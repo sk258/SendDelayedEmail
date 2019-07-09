ffb7bc3a1f638b44216b3b3e4e5807fc0f0c9b23
sk258 <sk258@snu.edu.in>
2019-07-10 01:53:15 +0530

    Update SendMailController.cs

--- SendDelayedEmail/Controllers/SendMailController.cs
+++ SendDelayedEmail/Controllers/SendMailController.cs
@@ -104,7 +104,7 @@ namespace SendDelayedEmail.Controllers
             smtp.Port = 25;
             smtp.UseDefaultCredentials = false;
             smtp.Credentials = new System.Net.NetworkCredential
-            ("satyendra.sharma.cse@gmail.com", "YOUme99!!");//your Smtp Email ID and Password
+            ("your_email_id", "password");//your Smtp Email ID and Password
             smtp.EnableSsl = true;
             try
             {
